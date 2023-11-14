5863ebce88ba5ee91c2987ae4f139e2a49d571db
jshohet <parykeeth90@gmail.com>
2023-11-13 19:41:12 -0500

    dist deleted to deploy

--- app/components/dist/Footer.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var react_1 = require("react");
-var Footer = function () {
-    return (react_1["default"].createElement("div", { className: "m-6" }, "\u00A9 2023 Josef Shohet"));
-};
-exports["default"] = Footer;
