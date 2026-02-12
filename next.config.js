fb59c701137ef0ed80d3ba3d10269c64f67223cb
jshohet <parykeeth90@gmail.com>
2026-02-12 08:45:44 -0500

    does this help

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+const nextConfig = { turbopack: {
+    // Set the root to the directory where next.config.js is located,
+    // or a parent directory if necessary.
+    root: path.join(__dirname, './'), 
+    // You might need to adjust './' to '../' or another path depending on your structure
+  },
+};
 module.exports = nextConfig
